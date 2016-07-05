f4f24fc454fdc9af06cc1989ace22bd333206a10
powerboat9 <powerboat9@users.noreply.github.com>
2016-07-05 18:12:13 -0400

    Update extract.c

--- extract.c
+++ extract.c
@@ -1,5 +1,10 @@
-#import <studio.h>
+#import <stdio.h>
 #import <string.h>
 
-void wmain() {
+void wmain(int argc, char **fname) {
+    char buffer[1024];
+    FILE *fptr;
+    fptr = fopen(&&fname, "w");
+    fseek(fptr, 0x3c, SEEK_SET);
+    fread(buffer, 
 }
