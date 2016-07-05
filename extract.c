a1f6f36fe9ed37ba041650269b7a0e682e287319
powerboat9 <powerboat9@users.noreply.github.com>
2016-07-05 18:23:19 -0400

    Update extract.c

--- extract.c
+++ extract.c
@@ -3,8 +3,11 @@
 
 void wmain(int argc, char **fname) {
     char buffer[1024];
+    
     FILE *fptr;
     fptr = fopen(&&fname, "w");
     fseek(fptr, 0x3c, SEEK_SET);
-    fread(buffer, 
+    fread(buffer, 4, 1, fptr);
+    if (memcmp(0x50450000, buffer, 4) == 0) {
+        
 }
