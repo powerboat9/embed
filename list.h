520106ad9539889ff5f9ce41b48368ee44719964
powerboat9 <powerboat9@users.noreply.github.com>
2016-07-06 01:11:13 -0400

    Update list.h

--- list.h
+++ list.h
@@ -1,9 +1,37 @@
+#import <stdlib.h>
+
 struct LISTELEMENT {
     char *k;
     char *v;
-} _attribut ((_packed))
+} _attribute((_packed))
 
 struct LIST {
     struct LISTELEMENT **elements;
     int size;
+    int pos;
 } _attribute((_packed))
+
+struct LIST* createList(int size) {
+    struct LIST *list;
+    if (size > 0) {
+        list = (struct LIST *) malloc(size * 4);
+        list.size = size;
+        list.pos = 0;
+    } else {
+        return (struct LIST *) NULL
+    }
+}
+
+void appendList(struct LIST *list, char *k, char *v) {
+    if (list) {
+        struct LISTELEMENT *element = list->elements + list->pos);
+        element.k = k;
+        element.v = v;
+    }
+}
+
+void compress(struct LIST *list)
+    if (list) {
+        struct LIST *newList = createList(list->size)
+        for (struct LISTELEMENT element = list->elements; element < (list->elements + list->pos); element++) {
+            element.
